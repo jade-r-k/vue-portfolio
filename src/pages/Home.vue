8735ff4d725fdcfa7ff6bea2d8d9ba06e3ea8a44
jade-r-k <jade.r.s.kelly@gmail.com>
2022-01-09 18:26:31 +0000

    Finishing style touches and hosted

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,16 +6,40 @@
         <p>This is a portfolio of my projects from college</p>
         <b-button variant="info" :to="{name: 'projects'}">My Projects</b-button>
         <br>
+        <br>
         <p class="float-right">Photo by <a
         href="https://unsplash.com/@nateggrant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nate
         Grant</a> on <a
         href="https://unsplash.com/s/photos/coding?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     </p>
       </b-jumbotron>
-    </div>
-    <div style="text-align: center;">
+      <b-row align-h="center">
+      <b-col>
+        <div style="text-align: center;">
       <h1 style="background-color:#343a40; color: white;">Contact</h1>
       <b-link href="https://ie.linkedin.com/in/jade-kelly-0b526b209" target="_blank">LinkedIn</b-link>
+    </div>
+      </b-col>
+      <b-col>
+        <div style="text-align: center;">
+      <h1 style="background-color:#343a40; color: white;">Preview</h1>
+        <b-carousel
+        id="carousel-1"
+      :interval="3000"
+      controls
+      indicators
+      background="#ababab"
+      img-blank-color
+      style="text-shadow: 3px 3px 4px #17a2b8;"
+      >
+          <b-carousel-slide v-for="project in projects" :key="project.id"
+          :caption="project.title"
+          :img-src="require('@/assets/images/' + project.image)">
+          </b-carousel-slide>
+        </b-carousel>
+        </div>
+      </b-col>
+    </b-row>
     </div>
   </div>
 </template>
@@ -23,7 +47,25 @@
 <script>
   export default {
     name: 'Home',
-    components: {}
+    components: {},
+    data() {
+      return {
+        projects: [],
+      }
+    },
+    mounted() {
+      this.getAllProjects()
+    },
+    methods: {
+      getAllProjects() {
+        fetch('./data/project.json')
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            this.projects = data
+          })
+      }
+    }
   }
 </script>
 
